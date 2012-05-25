b71570915b1efbaf00e1376f8a9a8303f41cb92b
Florentin Toulemon <ftoulemo@pc-gi-395.utbm.fr>
2012-05-25 16:05:58 +0200

    bleh

--- router/include/rssi/RSSI_list.h
+++ router/include/rssi/RSSI_list.h
@@ -3,6 +3,12 @@
 #ifndef __RSSI_LIST_h
 #define __RSSI_LIST_h
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <sys/time.h>
+
 typedef struct RssiList {
 	struct timeval rl_date ; // Expiration date
 	int rl_rssi_value ;
@@ -10,17 +16,19 @@ typedef struct RssiList {
 } RssiList ;
 
 typedef struct DeviceList {
-	char dl_mac_address [ 6 ] ;
+	unsigned char dl_mac_address [ 6 ] ;
 	RssiList * dl_rssi_list ;
 	struct DeviceList * dl_next ;
 } DeviceList ;
 
-void add_device (DeviceList ** l , char mac_addr [ 6 ]);
+DeviceList* add_device (DeviceList ** l , unsigned char mac_addr [ 6 ]);
 void clear_device_list (DeviceList ** l);
 void add_rssi_sample (DeviceList * l, int rssi_value);
 void clear_rssi_list (DeviceList * l);
+void delete_outdate (DeviceList ** l);
 void delete_outdated (DeviceList * l, struct timeval current_time);
-DeviceList* is_known(DeviceList ** l , char mac_addr [ 6 ]);
+DeviceList* is_known(DeviceList ** l , unsigned char  mac_addr [ 6 ]);
+void print (DeviceList** l);
 
 
 #endif /* __RSSI_LIST_h */
