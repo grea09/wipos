512fc8b364d5635bf3a405c848a85b7e9a02e4a1
ftoulemon <florentin.toulemon@gmail.com>
2012-06-08 14:45:53 +0200

    debut d'appli

--- mobile/src/fr/utbm/lo53/wipos/mobile/ui/Main.java
+++ mobile/src/fr/utbm/lo53/wipos/mobile/ui/Main.java
@@ -1,14 +1,31 @@
 package fr.utbm.lo53.wipos.mobile.ui;
 
-import fr.utbm.lo53.wipos.mobile.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import fr.utbm.lo53.wipos.mobile.R;
 
 public class Main extends Activity
 {
 	@Override
 	public void onCreate(Bundle bundle)
 	{
+		super.onCreate(bundle);
 		setContentView(R.layout.main);
+	}
+	
+	@Override
+	public void onStart(){
+		super.onStart();
+	}
+	
+	public void locate (View view) {
+		
+		
+	}
+	
+	public void mesure (View view) {
+		
+		
 	}
 }
