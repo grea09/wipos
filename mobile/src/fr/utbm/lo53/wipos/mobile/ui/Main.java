7427b3f287ae25aac6f964fb0e984c8ac20bbca3
ftoulemon <florentin.toulemon@gmail.com>
2012-06-08 15:24:21 +0200

    main

--- mobile/src/fr/utbm/lo53/wipos/mobile/ui/Main.java
+++ mobile/src/fr/utbm/lo53/wipos/mobile/ui/Main.java
@@ -1,12 +1,17 @@
 package fr.utbm.lo53.wipos.mobile.ui;
 
+import org.w3c.dom.Text;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import fr.utbm.lo53.wipos.mobile.R;
 
 public class Main extends Activity
 {
+	private TextView cursor;
+	
 	@Override
 	public void onCreate(Bundle bundle)
 	{
@@ -17,15 +22,18 @@ public class Main extends Activity
 	@Override
 	public void onStart(){
 		super.onStart();
+		cursor = (TextView) findViewById(R.id.cursor);
 	}
 	
 	public void locate (View view) {
-		
-		
+			
 	}
 	
 	public void mesure (View view) {
 		
+	}
+	
+	public void cancel (View view) {
 		
 	}
 }
